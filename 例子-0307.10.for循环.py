f2776a432ca7a23a68d76b3c1b15606c1739f31e
blackplusy <happyming07@sina.com>
2019-03-07 16:53:37 +0800

    Add files via upload

---
+++ 例子-0307.10.for循环.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+s='hello'
+for a in s:
+    print(a)
+
+l=[1,2,3,4,5]
+for i in l:
+    print(i)
+print('****')
+#range函数
+#range(10) 0---9
+#range(1,10)1--9
+
+for i in range(10):
+    print(i)
+
+for i in range(1,10):
+    print(i)
